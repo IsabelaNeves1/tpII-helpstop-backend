0367743b076c590cffef89a1d19aed9880406b12
IsabelaNeves1 <nevesisabela03@gmail.com>
2025-05-26 12:42:11 -0300

    Aula2605 - Command de Produto

--- HelpApp/HelpApp.Application/Products/Commands/ProductRemoveCommand.cs
+++ HelpApp/HelpApp.Application/Products/Commands/ProductRemoveCommand.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelpApp.Domain.Entities;
+using MediatR;
 
 namespace HelpApp.Application.Products.Commands
 {
-    internal class ProductRemoveCommand
+    public class ProductRemoveCommand : IRequest<Product>
     {
+        public int Id { get; set; }
+        public ProductRemoveCommand(int id)
+        {
+            Id = id;
+        }
     }
 }
